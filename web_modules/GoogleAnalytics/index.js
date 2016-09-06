5296c87864872d21f47f2ec597494434cb29533d
Philippe Gibert <philippe.gibert@gmail.com>
2016-09-06 14:18:58 +0200

    Disable Google Analytics on Dev

--- web_modules/GoogleAnalytics/index.js
+++ web_modules/GoogleAnalytics/index.js
@@ -1,6 +1,9 @@
 import React, { Component, PropTypes } from "react"
 import ReactGoogleAnalytics from "react-g-analytics"
 
+const isBrowser = (typeof window !== "undefined")
+const isProduction = process.env.NODE_ENV === "production"
+
 export default class GoogleAnalytics extends Component {
 
   static contextTypes = {
@@ -14,7 +17,9 @@ export default class GoogleAnalytics extends Component {
     } = this.context.metadata
 
     return (
-       <ReactGoogleAnalytics id={ pkg.config.GoogleAnalyticsId } />
+      ((isProduction && isBrowser) ?
+        <ReactGoogleAnalytics id={ pkg.config.GoogleAnalyticsId } />
+        : null)
     )
   }
 }
